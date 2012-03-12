b42d569a849d76d4ce025afe3bd992699ed8850e
Marlon Richert <marlon@vaadin.com>
2012-03-12 07:58:43 +0200

    generator(…) -> generate(…)

--- src/magna/carta/Comprehensions.java
+++ src/magna/carta/Comprehensions.java
@@ -4,40 +4,43 @@ import java.util.Iterator;
 
 public final class Comprehensions {
 
-	public static <T> Generator<T> generator(final Iterable<T> iterable) {
-		return generator(iterable, new Mapping<T,T>() {
-			public T apply(T input) {
-				return input;
-			}
-		});
-	}
-	
-	public static <IN,OUT> Generator<OUT> generator(final Iterable<IN> iterable, final Mapping<IN,OUT> mapping) {
-		return generator(iterable, mapping, new Filter<IN>() {
-			public boolean selects(IN item) {
-				return true;
-			}
-		});
-	}
-	
-	public static <IN,OUT> Generator<OUT> generator(final Iterable<IN> iterable, final Mapping<IN,OUT> mapping, final Filter<IN> filter) {
-		return new Generator<OUT>() {
-			Iterator<IN> iterator = iterable.iterator();
-			private IN next;
+    public static <T> Generator<T> generate(final Iterable<T> iterable) {
+        return generate(iterable, new Mapping<T, T>() {
+            public T apply(T input) {
+                return input;
+            }
+        });
+    }
 
-			public boolean hasNext() {
-				while (iterator.hasNext()) {
-					next = iterator.next();
-					if (filter.selects(next)) {
-						return true;
-					}
-				}
-				return false;
-			}
+    public static <IN, OUT> Generator<OUT> generate(
+            final Iterable<IN> iterable, final Mapping<IN, OUT> mapping) {
+        return generate(iterable, mapping, new Filter<IN>() {
+            public boolean selects(IN item) {
+                return true;
+            }
+        });
+    }
 
-			public OUT next() {
-				return mapping.apply(next);
-			}
-		};
-	}
+    public static <IN, OUT> Generator<OUT> generate(
+            final Iterable<IN> iterable, final Mapping<IN, OUT> mapping,
+            final Filter<IN> filter) {
+        return new Generator<OUT>() {
+            Iterator<IN> iterator = iterable.iterator();
+            private IN next;
+
+            public boolean hasNext() {
+                while (iterator.hasNext()) {
+                    next = iterator.next();
+                    if (filter.selects(next)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public OUT next() {
+                return mapping.apply(next);
+            }
+        };
+    }
 }
