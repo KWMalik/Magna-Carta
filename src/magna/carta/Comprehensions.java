c46b67d5a19a502a35dd33181b002d2fac69d14e
Marlon Richert <marlon@vaadin.com>
2012-03-12 08:07:20 +0200

    Converted anonymous to nested classes.

--- src/magna/carta/Comprehensions.java
+++ src/magna/carta/Comprehensions.java
@@ -1,26 +1,29 @@
 package magna.carta;
 
-
 public final class Comprehensions {
     public static <T> Generator<T> generate(Iterable<T> iterable) {
-        return generate(iterable, new Mapping<T, T>() {
-            public T apply(T input) {
-                return input;
-            }
-        });
+        return generate(iterable, new IdentityMapping<T>());
     }
 
     public static <IN, OUT> Generator<OUT> generate(Iterable<IN> iterable,
             Mapping<IN, OUT> mapping) {
-        return generate(iterable, mapping, new Filter<IN>() {
-            public boolean selects(IN item) {
-                return true;
-            }
-        });
+        return generate(iterable, mapping, new AllPassFilter<IN>());
     }
 
     public static <IN, OUT> Generator<OUT> generate(Iterable<IN> iterable,
             Mapping<IN, OUT> mapping, Filter<IN> filter) {
         return new Comprehension<IN, OUT>(iterable.iterator(), mapping, filter);
+    }
+
+    private static final class IdentityMapping<T> implements Mapping<T, T> {
+        public T apply(T input) {
+            return input;
+        }
+    }
+
+    private static final class AllPassFilter<IN> implements Filter<IN> {
+        public boolean selects(IN item) {
+            return true;
+        }
     }
 }
